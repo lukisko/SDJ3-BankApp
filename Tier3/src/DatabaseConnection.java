4a76d35954e3ecaeb0fa233c32f7448b3258c1f3
hanch <jurco10510@gmail.com>
2021-09-14 18:14:01 +0200

    database connection done

--- Tier3/src/DatabaseConnection.java
+++ Tier3/src/DatabaseConnection.java
@@ -1,2 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+  public Connection getConnection() throws SQLException {
+    DriverManager.registerDriver(new org.postgresql.Driver());
+    return DriverManager.getConnection( DatabaseCredentials.URL, DatabaseCredentials.NAME, DatabaseCredentials.PASSWORD);
+  }
 }
