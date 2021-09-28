c08a0c2a7fb4e74ba6877f1be999442be33e3742
nicol <nicolaecer@gmail.com>
2021-09-28 18:47:26 +0200

    Callback feature/ IGeneral interface update

--- Customers/src/RunMethodCustomer.java
+++ Customers/src/RunMethodCustomer.java
@@ -7,19 +7,40 @@ import java.util.Scanner;
 public class RunMethodCustomer {
 
 
-
-  public static void main(String[] args) throws RemoteException
-  {
-    Client client=new Client();
-
-    Scanner input=new Scanner(System.in);
-
-   int id= client.getMyID("George Washington");
-      client.takeMoney(id,300);
+    public static void main(String[] args) throws RemoteException, InterruptedException {
+        Client client = new Client();
+        Scanner input = new Scanner(System.in);
 
 
+        System.out.println("Please wait...");
+        Thread.sleep(3000);
+        System.out.println("Login\n Insert your name:\n");
 
-//    client.takeMoney(1,3);
-//    client.disconnect();
-  }
+        String clientName = input.nextLine();
+        int customerID = client.getMyID(clientName);
+
+        while (true) {
+            System.out.println("Please choose an action\n 0)Exit \n 1)Check balance \n 2)Withdraw money\n ");
+            int action = input.nextInt();
+
+            if (action == 0) {
+                client.disconnect();
+                break;
+            }
+
+            switch (action) {
+                case 1:
+                    System.out.println("Your balance is: " + client.checkAmount(customerID));
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("Insert the amount that you want to withdraw ");
+                    int amountToWithdraw = input.nextInt();
+                    client.takeMoney(customerID, amountToWithdraw);
+                    break;
+            }
+
+        }
+
+    }
 }
